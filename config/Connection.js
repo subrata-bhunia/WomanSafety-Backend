7bf84d2a05c90c9e7528bd1f5e3ee19ba610dcc5
subrata-bhunia <eerrcfgy@gmail.com>
2022-01-05 22:41:44 +0530

    Live Server Connect

--- config/Connection.js
+++ config/Connection.js
@@ -4,35 +4,20 @@ require('dotenv').config()
 
 
 const dbconfig= {
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    database:process.env.DB_NAME,
-    // password:''
+    host:process.env.DB_HOST_LIVE,
+    user:process.env.DB_USER_LIVE,
+    database:process.env.DB_NAME_LIVE,
+    password:process.env.DB_PASSWORD
 }
 
 
 var connection;
 
-function handleClose(){
-   connection= mysql.createConnection(dbconfig);
-    connection.connect(function(err) {              // The server is either down
-        if(err) {                                     // or restarting (takes a while sometimes).
-          console.log('error when connecting to db:', err);
-          setTimeout(handleClose, 2000); // We introduce a delay before attempting to reconnect,
-        }else{
-          console.log("MySql")
-        }                                    // to avoid a hot loop, and to allow our node script to
-      }); 
-
-      connection.on('error', function(err) {
-        console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-            handleClose();                         // lost due to either server restart, or a
-        } else {                                      // connnection idle timeout (the wait_timeout
-          throw err;                                  // server variable configures this)
-        }
-      });
-}
-handleClose();
+   connection= mysql.createConnection({
+    database:'u973746243_women_safety',
+    host:'109.106.254.101',
+    user:'u973746243_safety_root',
+    password:'Admin@123'
+   });
 
 module.exports = connection;
