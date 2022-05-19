da62a67b118bc3d9354474d7a3fd8f64d85a5990
subrata-bhunia <eerrcfgy@gmail.com>
2022-05-19 14:32:54 +0530

    authentication complete

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -1,154 +1,158 @@
 const {
-    create,
-    getUserByUserEmail,
-    getUserByUserId,
-    getUsers,
-    updateUser,
-    deleteUser
- } = require("./user.service");
+  create,
+  getUserByUserEmail,
+  getUserByUserId,
+  getUsers,
+  updateUser,
+  deleteUser,
+} = require("./user.service");
 const { hashSync, genSaltSync, compareSync } = require("bcrypt");
 const { sign } = require("jsonwebtoken");
 
 module.exports = {
-    createUser: (req, res) => {
-        const body = req.body;
-        const salt = genSaltSync(10);
-        body.password = hashSync(body.password, salt);
-        // body.c_password = hashSync(body.c_password, salt);
-        create(body, (err, results) => {
-            if (err) {
-                console.log(err);
-                return res.status(500).json({
-                    success: 0,
-                    message: "Database connection errror"
-                });
-            }
-            return res.status(200).json({
-                success: 1,
-                message: "User Create Successful."
-            });
+  createUser: (req, res) => {
+    const body = req.body;
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
+    // body.c_password = hashSync(body.c_password, salt);
+    create(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database connection errror",
         });
-    },
+      }
+      return res.status(200).json({
+        success: 1,
+        message: "User Create Successful.",
+      });
+    });
+  },
 
-    getUsers: (req, res) => {
-        getUsers((err, results) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            return res.json({
+  getUsers: (req, res) => {
+    getUsers((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      return res.json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  getUserByUserId: (req, res) => {
+    const id = req.params.id;
+    getUserByUserId(id, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (!results) {
+        return res.json({
+          success: 0,
+          message: "Record not Found",
+        });
+      }
+      results.password = undefined;
+      return res.json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  getUsers: (req, res) => {
+    getUsers((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      return res.json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  updateUsers: (req, res) => {
+    const body = req.body;
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
+    updateUser(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (results === 0) {
+        return res.json({
+          success: 0,
+          data: "failed to update user",
+        });
+      }
+      return res.json({
+        success: 1,
+        message: "updated successfully",
+      });
+    });
+  },
+  deleteUser: (req, res) => {
+    const data = req.body;
+    deleteUser(data, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (!results) {
+        return res.json({
+          success: 0,
+          message: "Record Not Found",
+        });
+      }
+      return res.json({
+        success: 1,
+        message: "user deleted successfully",
+      });
+    });
+  },
+  login: (req, res) => {
+    const body = req.body;
+    getUserByUserEmail(body.email, (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      if (!results) {
+        return res.json({
+          success: 0,
+          data: "Invalid name or password",
+        });
+      }
 
-                success: 1,
-                data: results
-            });
+      //    const result = compareSync(body.password, results.password);
+      const salt = genSaltSync(10);
+      if (results) {
+        // console.log(hashSync(body.password, salt));
+        if (compareSync(body.password, results.password)) {
+          console.log("passs");
+          const jsontoken = sign({ result: results }, "qwe1234", {
+            expiresIn: "1h",
+          });
+          return res.json({
+            success: 1,
+            message: "login successfully",
+            token: jsontoken,
+          });
+        } else {
+          return res.json({
+            success: 0,
+            data: "Invalid password",
+          });
+        }
+      } else {
+        return res.json({
+          success: 0,
+          data: "Invalid name or password",
         });
-
-    },
-    getUserByUserId: (req, res) => {
-        const id = req.params.id;
-        getUserByUserId(id, (err, results) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            if (!results) {
-                return res.json({
-                    success: 0,
-                    message: "Record not Found"
-                });
-            }
-            results.password = undefined;
-            return res.json({
-                success: 1,
-                data: results
-            });
-        });
-    },
-    getUsers: (req, res) => {
-        getUsers((err, results) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            return res.json({
-                success: 1,
-                data: results
-            });
-        });
-    },
-    updateUsers: (req, res) => {
-        const body = req.body;
-         const salt = genSaltSync(10);
-         body.password = hashSync(body.password, salt);
-        updateUser(body, (err, results) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            if (results===0) {
-                return res.json({
-                    success: 0,
-                    data: "failed to update user"
-                });
-            }
-            return res.json({
-                success: 1,
-                message: "updated successfully"
-            });
-        });
-    },
-    deleteUser: (req, res) => {
-        const data = req.body;
-        deleteUser(data, (err, results) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            if (!results) {
-                return res.json({
-                    success: 0,
-                    message: "Record Not Found"
-                });
-            }
-            return res.json({
-                success: 1,
-                message: "user deleted successfully"
-            });
-        });
-    },
-    login: (req, res) => {
-        const body = req.body;
-        getUserByUserEmail(body.full_name, (err, results) => {
-            if (err) {
-                console.log(err);
-            }
-            if (!results) {
-                return res.json({
-                    success: 0,
-                    data: "Invalid name or password"
-                });
-            }
-          
-        //    const result = compareSync(body.password, results.password);
-            
-            
-                if (results)
-                {
-                // results.password = undefined;
-                const jsontoken = sign({ result: results }, "qwe1234", {
-                    expiresIn: "1h"
-                });
-                return res.json({
-                    success: 1,
-                    message: "login successfully",
-                    token: jsontoken
-                });
-            } else {
-                return res.json({
-                    success: 0,
-                    data: "Invalid name or password"
-                });
-            }
-        });
-    },
-}
+      }
+    });
+  },
+};
--- api/users/user.service.js
+++ api/users/user.service.js
@@ -1,97 +1,97 @@
 const connection = require("../../config/Connection");
 
-
 // --------------------- //
-const createUser= 'INSERT INTO `users` (`user_id`, `full_name`, `email`, `phone`, `password`, `aadhar_card`,`photo`,`verified`) VALUES (?,?,?,?,?,?,?,?)'
+const createUser =
+  "INSERT INTO `users` (`user_id`, `full_name`, `email`, `phone`, `password`, `aadhar_card`,`photo`,`verified`) VALUES (?,?,?,?,?,?,?,?)";
 module.exports = {
-    create: (data, callBack) => {
-      connection.query(createUser,
-        [
-          Math.random()*9999999,
-          data.full_name,
-          data.email,
-          data.phone,
-          data.password,
-          data.aadhar_card,
-          data.photo,
-          data.verified
-        ],
-        (error, results, fields) => {
-          if (error) {
-            callBack(error);
-          }
-          return callBack(null, results);
+  create: (data, callBack) => {
+    connection.query(
+      createUser,
+      [
+        Math.random() * 9999999,
+        data.full_name,
+        data.email,
+        data.phone,
+        data.password,
+        data.aadhar_card,
+        data.photo,
+        data.verified,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
         }
-    
-      );
-    },
-  
-    getUsers: callBack => {
-      connection.query(
-        `select user_id,full_name,aadhar_card,phone,password from users`,
-        [],
-        (error, results, fields) => {
-          if (error) {
-            callBack(error);
-          }
-          return callBack(null, results);
-        }
-      );
-    },
-    getUserByUserEmail: (full_name, callBack) => {
-        connection.query(
-          `select * from users where full_name = ?`,
-          [full_name],
-          (error, results, fields) => {
-            if (error) {
-              callBack(error);
-            }
-            return callBack(null, results[0]);
-          }
-        );
-      },
-      getUserByUserId: (id, callBack) => {
-        connection.query(
-          `select * from users where user_id = ?`,
-          [id],
-          (error, results, fields) => {
-            if (error) {
-              callBack(error);
-            }
-            return callBack(null, results[0]);
-          }
-        );
-      },
-      updateUser: (data, callBack) => {
-        connection.query(
-          `update users set full_name=?,aadhar_card=?,phone=?,password=?,photo=?,last_location=? where user_id=?`,
-          [
-            data.name,
-            data.aadhar_card,
-            data.number,
-            data.password,
-            data.photo,
-            data.last_location,
-            data.id
-          ],
-          (error, results, fields) => {
-            if (error) {
-              callBack(error);
-            }
-            return callBack(null, results.affectedRows);
-          }
-        );
-      },
-      deleteUser: (data, callBack) => {
-        connection.query(
-          `delete from users where user_id = ?`,
-          [data.id],
-          (error, results, fields) => {
-            if (error) {
-              callBack(error);
-            }
-            return callBack(null, results[0]);
-          }
-        );
+        return callBack(null, results);
       }
-  }
+    );
+  },
+
+  getUsers: (callBack) => {
+    connection.query(
+      `select user_id,full_name,aadhar_card,phone,password from users`,
+      [],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  getUserByUserEmail: (email, callBack) => {
+    connection.query(
+      `select * from users where email = ?`,
+      [email],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  getUserByUserId: (id, callBack) => {
+    connection.query(
+      `select * from users where user_id = ?`,
+      [id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  updateUser: (data, callBack) => {
+    connection.query(
+      `update users set full_name=?,aadhar_card=?,phone=?,password=?,photo=?,last_location=? where user_id=?`,
+      [
+        data.name,
+        data.aadhar_card,
+        data.number,
+        data.password,
+        data.photo,
+        data.last_location,
+        data.id,
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results.affectedRows);
+      }
+    );
+  },
+  deleteUser: (data, callBack) => {
+    connection.query(
+      `delete from users where user_id = ?`,
+      [data.id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+};
