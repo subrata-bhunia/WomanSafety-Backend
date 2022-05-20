8089248b43b483cc7784d4c4770b5040859262f9
subrata-bhunia <eerrcfgy@gmail.com>
2022-05-20 11:40:50 +0530

    some issue fix

--- api/sos_contact/sos_contact.controller.js
+++ api/sos_contact/sos_contact.controller.js
@@ -1,3 +1,5 @@
+const { updateCount } = require("../circles/circles.controller");
+const { updateCounts, getCount } = require("../circles/circles.service");
 const { getAllContact, addContacts } = require("./sos_contact.service");
 
 module.exports = {
@@ -30,11 +32,34 @@ module.exports = {
           success: 0,
           error: err,
         });
+      } else {
+        var circle_id = body.circle_id;
+        getCount(circle_id, (err, rst) => {
+          if (err) {
+            return res.json({
+              success: 0,
+              data: "Something went to wrong",
+            });
+          } else {
+            var count = rst.count;
+            console.log(typeof count);
+            updateCounts({ circle_id, count: count + 1 }, (error, response) => {
+              if (error) {
+                console.log(error);
+                return res.json({
+                  success: 0,
+                  data: "Something went to wrong",
+                });
+              } else {
+                return res.json({
+                  success: 1,
+                  data: "SOS contact added successfully",
+                });
+              }
+            });
+          }
+        });
       }
-      return res.json({
-        success: 1,
-        data: "Contact added successfully",
-      });
     });
   },
   // deleteCircle: (req, res) => {
